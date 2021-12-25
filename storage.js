bfde5d98e6e768ec751c44e21daeffc0c5440d25
Cyril Gupta <5773498+cyrilgupta@users.noreply.github.com>
2021-12-25 21:36:40 +0400

    Setup

--- storage.js
+++ storage.js
@@ -1,1 +1,41 @@
+const fs = require('fs')
+const path = require('path')
 
+class Storage {
+  fileName;
+
+  constructor(fileN) {
+    this.fileName = fileN;.
+  }
+
+  get(key) {
+    let pth = this.fileName;
+
+    if (fs.existsSync(pth)) {
+      let rawdata = fs.readFileSync(pth)
+
+      if (rawdata != '') {
+        let data = JSON.parse(rawdata)
+        return data[key]
+      } else return null
+    } else {
+      return null
+    }
+  }
+
+  set(key, value) {
+    let pth = this.fileName;
+
+    let data = {}
+    if (fs.existsSync(pth)) {
+      let rawdata = fs.readFileSync(pth)
+      if (rawdata != '') data = JSON.parse(rawdata)
+    }
+    data[key] = value
+
+    fs.writeFileSync(pth, JSON.stringify(data))
+    return true
+  }
+}
+
+module.exports = { Storage };
